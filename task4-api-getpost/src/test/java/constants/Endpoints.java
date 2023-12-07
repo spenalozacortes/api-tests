a161b8f50ed6fa1b3bba08671c3fd47ead932baf
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-12-07 10:45:28 -0600

    Reuse constants

--- task4-api-getpost/src/test/java/constants/Endpoints.java
+++ task4-api-getpost/src/test/java/constants/Endpoints.java
@@ -7,6 +7,6 @@ public class Endpoints {
 
     public static final String POSTS = "/posts";
     public static final String USERS = "/users";
-    public static final String POST_BY_ID = String.format("/posts/{%s}", Parameters.ID);
-    public static final String USER_BY_ID = String.format("/users/{%s}", Parameters.ID);
+    public static final String POST_BY_ID = String.format("%s/{%s}", POSTS, Parameters.ID);
+    public static final String USER_BY_ID = String.format("%s/{%s}", USERS, Parameters.ID);
 }
