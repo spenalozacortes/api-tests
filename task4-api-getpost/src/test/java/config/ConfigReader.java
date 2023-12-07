0db203af112480da4f799990cacf1cbe178342ec
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-12-07 10:36:00 -0600

    Use try-with-resources

--- task4-api-getpost/src/test/java/config/ConfigReader.java
+++ task4-api-getpost/src/test/java/config/ConfigReader.java
@@ -12,10 +12,8 @@ public class ConfigReader {
     private final JsonObject environment;
 
     public ConfigReader(String path) {
-        try {
-            FileReader reader = new FileReader(path);
+        try (FileReader reader = new FileReader(path)) {
             environment = JsonParser.parseReader(reader).getAsJsonObject();
-            reader.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
